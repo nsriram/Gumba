439a8851649c335a6b90a1892d52d3946bc00471
Sriram Iyengar <narasis@thoughtworks.com>
2012-03-02 20:14:24 +0530

    Initial Commit

---
+++ TechRadar/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TechRadar
+//
+//  Created by Sriram Narasimhan on 02/03/12.
+//  Copyright (c) 2012 ThoughtWorks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ TechRadar/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TechRadar
+//
+//  Created by Sriram Narasimhan on 02/03/12.
+//  Copyright (c) 2012 ThoughtWorks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
