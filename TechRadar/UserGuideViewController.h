700219ae773b7cdcfd8be0994e5ade840bbf750b
Sriram Narasimhan <narasis@thoughtworks.com>
2012-10-16 00:21:19 +0530

    Sriram:Making userguide to use pagecontrol. In Progress

--- TechRadar/UserGuideViewController.h
+++ TechRadar/UserGuideViewController.h
@@ -1,5 +1,11 @@
 #import <UIKit/UIKit.h>
 
-@interface UserGuideViewController : UIViewController
-
+@interface UserGuideViewController : UIViewController<UIScrollViewDelegate> {
+    UIScrollView* scrollView;
+    UIPageControl* pageControl;
+}
+@property (nonatomic, strong) IBOutlet UIScrollView* scrollView;
+@property (nonatomic, strong) IBOutlet UIPageControl* pageControl;
+@property (nonatomic, assign) BOOL pageControlBeingUsed;
+- (IBAction)changePage;
 @end
--- TechRadar/UserGuideViewController.m
+++ TechRadar/UserGuideViewController.m
@@ -2,35 +2,61 @@
 #import "AppConstants.h"
 
 @interface UserGuideViewController ()
-
 @end
 
 @implementation UserGuideViewController
-
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    return self;
-}
-
-- (void)swipeRight:(UIPinchGestureRecognizer *)recognizer  {
-    [self.navigationController popViewControllerAnimated:TRUE];
-}
-
--(void) bindSwipeRight {
-    UISwipeGestureRecognizer *recognizer = [[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(swipeRight:)];
-    [recognizer setDirection:(UISwipeGestureRecognizerDirectionRight)];
-    [[self view] addGestureRecognizer:recognizer];
-}
+@synthesize scrollView,pageControl,pageControlBeingUsed;
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     [self.view setBackgroundColor:[AppConstants detailBackgroundColor]];
-    [self bindSwipeRight];
-}
+    pageControlBeingUsed = NO;
 
+    NSArray *images = [NSArray arrayWithObjects:@"people", @"background", @"About Radar", nil];
+
+    for (int i = 0; i < images.count; i++) {
+        CGRect frame;
+        frame.origin.x = self.scrollView.frame.size.width * i;
+        frame.origin.y = 0;
+        frame.size = self.scrollView.frame.size;
+        
+        UIView *subview = [[UIView alloc] initWithFrame:frame];
+        NSString *fullpath = [[NSBundle mainBundle] pathForResource:[images objectAtIndex:i] ofType:@"jpg"];
+
+        UIImage *img = [UIImage imageWithContentsOfFile:fullpath];
+        UIImageView *imgView = [[UIImageView alloc] initWithFrame:frame];
+        imgView.image=img;
+
+        [subview addSubview:imgView];
+        [self.scrollView addSubview:subview];
+    }
+    self.scrollView.contentSize = CGSizeMake(self.scrollView.frame.size.width * images.count, self.scrollView.frame.size.height);
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
 }
 
+- (void)scrollViewDidScroll:(UIScrollView *)sender {
+    CGFloat pageWidth = self.scrollView.frame.size.width;
+    int page = floor((self.scrollView.contentOffset.x - pageWidth / 2) / pageWidth) + 1;
+    self.pageControl.currentPage = page;
+}
+
+- (IBAction)changePage {
+    CGRect frame;
+    frame.origin.x = self.scrollView.frame.size.width * self.pageControl.currentPage;
+    frame.origin.y = 0;
+    frame.size = self.scrollView.frame.size;
+    [self.scrollView scrollRectToVisible:frame animated:YES];
+    pageControlBeingUsed=YES;
+}
+
+- (void)scrollViewWillBeginDragging:(UIScrollView *)scrollView {
+    pageControlBeingUsed = NO;
+}
+
+- (void)scrollViewDidEndDecelerating:(UIScrollView *)scrollView {
+    pageControlBeingUsed = NO;
+}
 @end
