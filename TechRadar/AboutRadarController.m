b65132c619fcc1fab8448a5b600808620ad63668
Magnus Ernstsson <magnus@ernstsson.net>
2012-10-25 16:01:28 +0100

    Added introduction view, changed about image loading

--- TechRadar/AboutRadarController.m
+++ TechRadar/AboutRadarController.m
@@ -10,34 +10,6 @@
     return self;
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-}
-
-- (void)swipeRight:(UIPinchGestureRecognizer *)recognizer  {
-    [self.navigationController popViewControllerAnimated:TRUE];
-}
-
--(void) bindSwipeRight {
-    UISwipeGestureRecognizer *recognizer = [[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(swipeRight:)];
-    [recognizer setDirection:(UISwipeGestureRecognizerDirectionRight)];
-    [[self view] addGestureRecognizer:recognizer];
-
-    UITapGestureRecognizer *singleTap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(swipeRight:)] ;
-    singleTap.numberOfTapsRequired = 1;
-    [[self view] addGestureRecognizer:singleTap];
-}
-
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    self.view.backgroundColor = [[UIColor alloc] initWithPatternImage:[UIImage imageNamed:@"about_radar.png"]];
-    [self bindSwipeRight];
-}
-
-- (void)viewDidUnload {
-    [super viewDidUnload];
-}
-
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
     return UIInterfaceOrientationIsPortrait(interfaceOrientation);
 }
