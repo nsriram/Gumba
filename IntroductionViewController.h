29b489c38b7c352e039d52d6399f01adc11894af
Sriram Narasimhan <sriram.iyengar78@gmail.com>
2013-06-08 09:37:46 +0530

    Sriram: Fixed the introduction tab to load every radar trends and highlights as html

--- IntroductionViewController.h
+++ IntroductionViewController.h
@@ -1,12 +1,7 @@
-//
-//  IntroductionViewController.h
-//  TechRadar
-//
-//  Created by Magnus Ernstsson on 2012-10-25.
-//  Copyright (c) 2012 ThoughtWorks. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
 
-@interface IntroductionViewController : UIViewController
+@interface IntroductionViewController : UIViewController{
+    UIWebView* trendsWebView;
+}
+@property (nonatomic,strong) IBOutlet UIWebView  *trendsWebView;
 @end
--- IntroductionViewController.m
+++ IntroductionViewController.m
@@ -1,11 +1,3 @@
-//
-//  IntroductionViewController.m
-//  TechRadar
-//
-//  Created by Magnus Ernstsson on 2012-10-25.
-//  Copyright (c) 2012 ThoughtWorks. All rights reserved.
-//
-
 #import "IntroductionViewController.h"
 #import "PreviewViewController.h"
 
@@ -14,9 +6,9 @@
 @end
 
 @implementation IntroductionViewController
+@synthesize trendsWebView;
 
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
-{
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
     if (self) {
         // Custom initialization
@@ -24,30 +16,28 @@
     return self;
 }
 
--(IBAction) faq:(UIButton*)button {
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    NSString *htmlBundle = [[NSBundle mainBundle] pathForResource:@"trends" ofType:@"html"];
+    [trendsWebView loadRequest:[NSURLRequest requestWithURL:[NSURL fileURLWithPath:htmlBundle isDirectory:NO]]];
+}
+
+- (IBAction)faq:(UIButton *)button {
     [self performSegueWithIdentifier:@"intro-to-preview" sender:@"http://martinfowler.com/articles/radar-faq.html"];
 }
 
--(IBAction) radar:(UIButton*)button {
+- (IBAction)radar:(UIButton *)button {
     [self performSegueWithIdentifier:@"intro-to-preview" sender:@"http://www.thoughtworks.com/radar"];
 }
 
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(NSString*)url
-{
-	if ([segue.identifier isEqualToString:@"intro-to-preview"]) {
-    	PreviewViewController *controller = (PreviewViewController*)segue.destinationViewController;
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(NSString *)url {
+    if ([segue.identifier isEqualToString:@"intro-to-preview"]) {
+        PreviewViewController *controller = (PreviewViewController *) segue.destinationViewController;
         controller.url = url;
-	}
+    }
 }
 
-- (void)viewDidLoad
-{
-    [super viewDidLoad];
-	// Do any additional setup after loading the view.
-}
-
-- (void)didReceiveMemoryWarning
-{
+- (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
